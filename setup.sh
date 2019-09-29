f64ff81152376d2d017de84608068e0ebf5f3ef4
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-09-29 09:14:16 -0700

    Install easy motion

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.vim/pack/default/start
+
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/default/start/vim-polyglot
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/default/start/vim-easymotion
