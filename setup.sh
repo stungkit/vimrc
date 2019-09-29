7dda5de5d6b72076daa1cba0d9ef648e41cb329e
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-09-29 11:34:26 -0700

    Install ack

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 mkdir -p ~/.vim/pack/default/start
 
+brew install ack
+
 cp .vimrc ~/.vimrc
 
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/default/start/vim-polyglot
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/default/start/vim-easymotion
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
 git clone https://github.com/kien/ctrlp.vim ~/.vim/pack/default/start/ctrlp.vim
+git clone https://github.com/mileszs/ack.vim ~/.vim/pack/default/start/ack.vim
