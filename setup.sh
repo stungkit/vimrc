5adfe2640a3761de0ea184186ca2cc61f4f8c2d6
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-09-29 11:22:06 -0700

    Copy vimrc

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 mkdir -p ~/.vim/pack/default/start
 
+cp .vimrc ~/.vimrc
+
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/default/start/vim-polyglot
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/default/start/vim-easymotion
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
