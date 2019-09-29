5128abcf945551496ce3661a51f8496a2bef5ef6
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-09-29 09:50:34 -0700

    Install control+p

--- .vimrc
+++ .vimrc
@@ -2,6 +2,7 @@ set number
 filetype plugin indent on
 syntax on
 set hlsearch
+set runtimepath^=~/.vim/pack/default/start/ctrlp.vim
 
 let mapleader=" "
 let g:EasyMotion_leader_key = '<Leader>'
--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ mkdir -p ~/.vim/pack/default/start
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/default/start/vim-polyglot
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/default/start/vim-easymotion
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+git clone https://github.com/kien/ctrlp.vim ~/.vim/pack/default/start/ctrlp.vim
