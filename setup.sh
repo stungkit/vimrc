93d3bc589b6b1e415737bdcae18910ff03805f03
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-09-29 22:04:41 -0700

    Install coc.nvim

--- setup.sh
+++ setup.sh
@@ -12,3 +12,30 @@ git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvb
 git clone https://github.com/kien/ctrlp.vim ~/.vim/pack/default/start/ctrlp.vim
 git clone https://github.com/mileszs/ack.vim ~/.vim/pack/default/start/ack.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/default/start/nerdtree
+
+
+## Install coc.nvim
+# Install latest nodejs
+if [ ! -x "$(command -v node)" ]; then
+	curl --fail -LSs https://install-node.now.sh/latest | sh
+	export PATH="/usr/local/bin/:$PATH"
+	# Or use apt-get
+	# sudo apt-get install nodejs
+fi
+
+mkdir -p ~/.vim/pack/default/start
+cd ~/.vim/pack/default/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv -
+
+# Copy extensions file
+mkdir -p ~/.config/coc/extensions
+cp package.json ~/.config/coc/extensions/package.json
+
+# Install extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+	echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
