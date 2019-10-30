773e07b8bc65c6a96b37213ea219a8a1de5b0962
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-10-30 15:30:32 -0700

    Refactor code

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.vim/pack/default/start
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALL_DIR=~/.vim/pack/default/start
 
-brew install ack
+mkdir -p ${INSTALL_DIR}
+
+brew install \
+	ack \
+	ag
 
 cp .vimrc ~/.vimrc
 
-git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/default/start/vim-polyglot
-git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/default/start/vim-easymotion
-git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
-git clone https://github.com/kien/ctrlp.vim ~/.vim/pack/default/start/ctrlp.vim
-git clone https://github.com/mileszs/ack.vim ~/.vim/pack/default/start/ack.vim
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/default/start/nerdtree
-git clone https://github.com/junegunn/fzf.vim ~/.vim/pack/default/start/fzf.vim
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+git clone https://github.com/sheerun/vim-polyglot ${INSTALL_DIR}/vim-polyglot
+git clone https://github.com/easymotion/vim-easymotion ${INSTALL_DIR}/vim-easymotion
+git clone https://github.com/morhetz/gruvbox.git ${INSTALL_DIR}/gruvbox
+git clone https://github.com/kien/ctrlp.vim ${INSTALL_DIR}/ctrlp.vim
+git clone https://github.com/mileszs/ack.vim ${INSTALL_DIR}/ack.vim
+git clone https://github.com/scrooloose/nerdtree.git ${INSTALL_DIR}/nerdtree
+git clone https://github.com/junegunn/fzf.vim ${INSTALL_DIR}/fzf.vim
 
 ## Install coc.nvim
 ${DIR}/coc.nvim/setup.sh
