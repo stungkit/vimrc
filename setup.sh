d6902789c7b50ac72f2aa835fb35071528077830
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-11-08 23:42:40 -0800

    Install more plugins

--- setup.sh
+++ setup.sh
@@ -24,6 +24,9 @@ git -C ${INSTALL_DIR} clone https://github.com/tpope/vim-surround.git
 git -C ${INSTALL_DIR} clone https://github.com/nathanaelkane/vim-indent-guides.git
 git -C ${INSTALL_DIR} clone https://github.com/airblade/vim-gitgutter.git
 git -C ${INSTALL_DIR} clone https://github.com/mechatroner/rainbow_csv.git
+git -C ${INSTALL_DIR} clone https://github.com/maxbrunsfeld/vim-yankstack.git
+git -C ${INSTALL_DIR} clone https://github.com/tpope/vim-fugitive.git
+git -C ${INSTALL_DIR} clone https://github.com/terryma/vim-expand-region.git
 
 git clone https://github.com/editorconfig/editorconfig-vim.git ${INSTALL_DIR}
 cp ${DIR}/.editorconfig ~/.editorconfig
