ec76dd91ec5f5451e7fb240dc1fdc75d8d0bd220
Smati Tungkitkancharoen <stungkit@gmail.com>
2019-12-17 21:57:50 -0800

    Remove bufferline

--- setup.sh
+++ setup.sh
@@ -30,7 +30,6 @@ git -C ${INSTALL_DIR} clone https://github.com/terryma/vim-expand-region.git
 git -C ${INSTALL_DIR} clone https://github.com/luochen1990/rainbow.git
 git -C ${INSTALL_DIR} clone https://github.com/junegunn/goyo.vim.git
 git -C ${INSTALL_DIR} clone https://github.com/mileszs/ack.vim
-git -C ${INSTALL_DIR} clone https://github.com/bling/vim-bufferline.git
 
 git -C ${INSTALL_DIR} clone https://github.com/editorconfig/editorconfig-vim.git
 cp ${DIR}/.editorconfig ~/.editorconfig
