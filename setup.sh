10102a274332b1cb9c29dbf50841f0ce82c04c94
stungkit <stungkit@gmail.com>
2020-01-07 10:51:45 -0800

    Install vim-commentary

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ git -C ${INSTALL_DIR} clone https://github.com/terryma/vim-expand-region.git
 git -C ${INSTALL_DIR} clone https://github.com/luochen1990/rainbow.git
 git -C ${INSTALL_DIR} clone https://github.com/junegunn/goyo.vim.git
 git -C ${INSTALL_DIR} clone https://github.com/mileszs/ack.vim
+git -C ${INSTALL_DIR} clone https://github.com/tpope/vim-commentary.git
 
 git -C ${INSTALL_DIR} clone https://github.com/editorconfig/editorconfig-vim.git
 cp ${DIR}/.editorconfig ~/.editorconfig
